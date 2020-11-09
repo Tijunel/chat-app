2cf4a4819dbf9e5d40f408574fbb585ab6b94052
Tijunel <justintijunel@gmail.com>
2020-11-09 16:10:52 -0700

    Added backend functionality

--- Routes/message.js
+++ Routes/message.js
@@ -4,7 +4,18 @@ const firebase = require('../Config/firebase');
 
 message.get('/', async (req, res) => {
     try {
-
+        const messagesRef = firebase.database().ref('messages');
+        var allMessages = [];
+        await messagesRef.once('value').then(snapshot => {
+            snapshot.forEach(child => {
+                allMessages.push({
+                    message: child.val().message,
+                    userID: child.val().userID,
+                    timestamp: child.val().timestamp
+                });
+            });
+        });
+        res.status(200).json(allMessages).end();
     } catch(e) {
         res.status(500).send('Error getting messages!').end();
     }
@@ -12,7 +23,17 @@ message.get('/', async (req, res) => {
 
 message.post('/', async (req, res) => {
     try {
-        res.status(200).end();
+        const message = req.body.message;
+        const userID = req.body.id;
+        const timestamp = Date.now();
+        const messagesRef = firebase.database().ref('messages');
+        const messageRef = messagesRef.child('message');
+        messageRef.push(message);
+        const userIDRef = messagesRef.child('userID');
+        userIDRef.push(userID);
+        const timestampRef = messagesRef.child('timestamp');
+        timestampRef.push(timestamp);
+        res.status(200).json({ timestamp: timestamp }).end();
     } catch(e) {
         res.status(500).send('Error posting message!').end();
     }
--- Routes/user.js
+++ Routes/user.js
@@ -1,42 +1,90 @@
 const express = require('express');
 const user = express.Router();
+const shortid = require('shortid');
 
 user.get('/', async (req, res) => {
     try {
-
-    } catch(e) {
+        const userRef = firebase
+            .database()
+            .ref('users')
+        var allUsers = [];
+        await userRef.once('value').then(snapshot => {
+            snapshot.forEach(child => {
+                allUsers.push({ 
+                    username: child.val().username, 
+                    colour: child.val().colour 
+                });
+            });
+        });
+        res.status(200).json(allUsers).end();
+    } catch (e) {
         res.status(500).send('Error creating user!').end();
     }
 });
 
 user.get('/id/:name', async (req, res) => {
     try {
-
-    } catch(e) {
+        const name = req.params.name;
+        const idRef = firebase.database().ref('users');
+        var userID = '';
+        await idRef.once('value').then(snapshot => {
+            snapshot.forEach(child => {
+                var id = child.key;
+                if (child.value.name === name) {
+                    userID = id;
+                    break;
+                }
+            });
+        });
+        res.status(200).json({ id: userID }).end();
+    } catch (e) {
         res.status(500).send('Error user ID!').end();
     }
 });
 
 user.post('/create', async (req, res) => {
     try {
+        const username = 'user-' + shortid.generate();
+        const colour = '#4FB3F7';
+        const usersRef = firebase.database().ref('users');
+        const usernameRef = usersRef.child('username');
+        usernameRef.push(username);
+        const colourRef = usersRef.child('colour');
+        colourRef.push(colour);
         res.status(200).end();
-    } catch(e) {
+    } catch (e) {
         res.status(500).send('Error creating user!').end();
     }
 });
 
 user.put('/name', async (req, res) => {
     try {
+        const name = req.body.username;
+        const userID = req.body.userID;
+        var usernameRef = firebase
+            .database()
+            .ref('users')
+            .child(userID)
+            .child('username');
+        usernameRef.set(name);
         res.status(200).end();
-    } catch(e) {
+    } catch (e) {
         res.status(500).send('Error changing username!').end();
     }
 });
 
 user.put('/colour', async (req, res) => {
     try {
+        const colour = req.body.colour;
+        const userID = req.body.userID;
+        var colourRef = firebase
+            .database()
+            .ref('users')
+            .child(userID)
+            .child('colour');
+        colourRef.set(colour);
         res.status(200).end();
-    } catch(e) {
+    } catch (e) {
         res.status(500).send('Error changing colour!').end();
     }
 });
